70d1d9869db105906d78adceee00e9d6450d429b
putra utama jaya <putra.utamajaya@ematicsolutions.com>
2020-03-01 12:04:55 +0700

    Updates

---
+++ precache-manifest.47eec340a149dee64f031e49c8c1e47d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55d7a936eb61df63929c60bdb86c2ce",
+    "url": "/fabelio-test/index.html"
+  },
+  {
+    "revision": "c81efe30a9b816cb92e6",
+    "url": "/fabelio-test/static/css/main.e70697c9.chunk.css"
+  },
+  {
+    "revision": "1fb444d939814094b607",
+    "url": "/fabelio-test/static/js/2.5d189abc.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/fabelio-test/static/js/2.5d189abc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81efe30a9b816cb92e6",
+    "url": "/fabelio-test/static/js/main.83c50b86.chunk.js"
+  },
+  {
+    "revision": "79ed562b79c83e81b64d",
+    "url": "/fabelio-test/static/js/runtime-main.bc74bc0b.js"
+  }
+]);
